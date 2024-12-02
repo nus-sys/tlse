0296ed141cc9ed0a870585fb017e4b4c01803a8a
sgdxbc <guangda@u.nus.edu>
2024-12-02 02:03:10 +0000

    Add building script and basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn init() {
+        unsafe { tls_init() }
+    }
+
+    #[test]
+    fn create_context() {
+        let context = unsafe { tls_create_context(1, TLS_V12 as _) };
+        assert!(!context.is_null());
+        unsafe { tls_destroy_context(context) }
     }
 }
